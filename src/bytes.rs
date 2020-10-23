680427428901370585d18a4bbd4d796f5b667b14
Alexey Voznyuk <me@swizard.info>
2020-10-23 13:49:33 +0300

    `BytesPool` refactoring

--- src/bytes.rs
+++ src/bytes.rs
@@ -7,4 +7,18 @@ use super::{
 
 pub type Bytes = Shared<Vec<u8>>;
 pub type BytesMut = Unique<Vec<u8>>;
-pub type BytesPool = pool::Pool<Vec<u8>>;
+
+#[derive(Clone, Debug)]
+pub struct BytesPool {
+    pool: pool::Pool<Vec<u8>>,
+}
+
+impl BytesPool {
+    pub fn new() -> BytesPool {
+        BytesPool { pool: pool::Pool::new(), }
+    }
+
+    pub fn lend(&self) -> BytesMut {
+        self.pool.lend(Vec::new)
+    }
+}
